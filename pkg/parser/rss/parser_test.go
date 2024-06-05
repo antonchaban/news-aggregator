a3514cebd6c376cf57f7b875095969131b864a08
Anton Chaban <a.chaban-2027@kpi.ua>
2024-06-05 11:43:42 +0300

    Add tests for RSS parser

--- pkg/parser/rss/parser_test.go
+++ pkg/parser/rss/parser_test.go
@@ -5,24 +5,75 @@ import (
 	"os"
 	"reflect"
 	"testing"
+	"time"
 )
 
 func TestParser_ParseFile(t *testing.T) {
-	type args struct {
-		f *os.File
-	}
 	tests := []struct {
-		name    string
-		args    args
-		want    []model.Article
-		wantErr bool
+		name     string
+		fileName string
+		want     []model.Article
+		wantErr  bool
 	}{
-		// TODO: Add test cases.
+		{
+			name:     "Valid RSS",
+			fileName: "valid_rss.xml",
+			want: []model.Article{
+				{
+					Title:       "Article 1",
+					Link:        "http://example.com/article1",
+					Description: "This is the first article.",
+					Source:      "Sample Feed",
+					PubDate:     time.Date(2006, time.January, 2, 15, 4, 5, 0, time.UTC),
+				},
+				{
+					Title:       "Article 2",
+					Link:        "http://example.com/article2",
+					Description: "This is the second article.",
+					Source:      "Sample Feed",
+					PubDate:     time.Date(2006, time.January, 3, 15, 4, 5, 0, time.UTC),
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name:     "Invalid RSS",
+			fileName: "invalid_rss.xml",
+			want:     nil,
+			wantErr:  true,
+		},
+		{
+			name:     "Empty RSS",
+			fileName: "empty_rss.xml",
+			want:     []model.Article{},
+			wantErr:  false,
+		},
+		{
+			name:     "Missing Fields RSS",
+			fileName: "missing_fields_rss.xml",
+			want: []model.Article{
+				{
+					Title:  "Article 1",
+					Link:   "http://example.com/article1",
+					Source: "Sample Feed",
+				},
+				{
+					Title:       "Article 2",
+					Link:        "http://example.com/article2",
+					Description: "This is the second article.",
+					Source:      "Sample Feed",
+				},
+			},
+			wantErr: false,
+		},
 	}
+
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			r := &Parser{}
-			got, err := r.ParseFile(tt.args.f)
+			f := loadTestData(t, tt.fileName)
+			defer f.Close()
+			got, err := r.ParseFile(f)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("ParseFile() error = %v, wantErr %v", err, tt.wantErr)
 				return
@@ -32,4 +83,12 @@ func TestParser_ParseFile(t *testing.T) {
 			}
 		})
 	}
+}
+
+func loadTestData(t *testing.T, filename string) *os.File {
+	file, err := os.Open("testdata/" + filename)
+	if err != nil {
+		t.Fatalf("failed to open test data file %s: %v", filename, err)
+	}
+	return file
 }
