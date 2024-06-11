89700d2f6723b964ce5c5a6781fc75f2ec025b8a
Anton Chaban <a.chaban-2027@kpi.ua>
2024-06-11 16:01:36 +0300

    Update doc for storage layer

--- pkg/storage/doc.go
+++ pkg/storage/doc.go
@@ -1,11 +1,8 @@
-// Package storage implements the storage layer of the application.
-// It contains the database operations for the application.
-// It interacts with the model layer to perform CRUD operations on the database.
-// It provides an abstraction over the database operations to the service layer.
+// Package storage provides data storage and retrieval functionalities.
+// It includes various implementations to handle data persistence,
+// offering an abstraction layer over the underlying storage mechanisms.
+// This layer responsible for interacting with the database and performing operations with data.
 //
-// The storage layer should be used by the service layer to perform database operations.
-//
-// The storage layer contains the following packages:
-//
-// - article_inmemory: contains the in-memory database implementation for the article storage
+// The storage package should be used by other layers or components to perform
+// data storage and retrieval operations, providing a clean separation of concerns.
 package storage
---
+++ pkg/storage/inmemory/doc.go
@@ -0,0 +1,7 @@
+// Package inmemory provides an in-memory data storage implementation.
+// This package is designed for scenarios where a lightweight, fast, and
+// transient storage solution is required. It is suitable for testing,
+// development, and applications that do not require persistent storage.
+//
+// Note: As this package stores data in memory, all stored data will be lost when the application is stopped.
+package inmemory
