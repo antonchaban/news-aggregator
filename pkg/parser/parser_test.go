64e4d805c63e8e26031319328ca72b7740d14c36
Anton Chaban <a.chaban-2027@kpi.ua>
2024-06-27 18:52:15 +0300

    Add tests for parser.go and format.go

--- pkg/parser/parser_test.go
+++ pkg/parser/parser_test.go
@@ -2,14 +2,63 @@ package parser
 
 import (
 	"github.com/antonchaban/news-aggregator/pkg/model"
-	"github.com/stretchr/testify/require"
+	"github.com/antonchaban/news-aggregator/pkg/parser/json"
+	"github.com/antonchaban/news-aggregator/pkg/parser/rss"
+	"net/url"
+	"reflect"
 	"testing"
 	"time"
 )
 
-func TestParseArticlesFromFiles(t *testing.T) {
+func TestParseArticlesFromFeed(t *testing.T) {
 	type args struct {
-		files []string
+		urlPath url.URL
+	}
+	rssURL, _ := url.Parse("http://rss.cnn.com/rss/cnn_topstories.rss")
+	tests := []struct {
+		name    string
+		args    args
+		want    []model.Article
+		wantErr bool
+	}{
+		{
+			name: "rss",
+			args: args{
+				urlPath: *rssURL,
+			},
+			want: []model.Article{
+				{
+					Title:       "Some on-air claims about Dominion Voting Systems were false, Fox News acknowledges in statement after deal is announced",
+					Description: "",
+					Link:        "https://www.cnn.com/business/live-news/fox-news-dominion-trial-04-18-23/index.html",
+					Source: model.Source{
+						Name: "CNN.com - RSS Channel - HP Hero",
+						Link: "http://rss.cnn.com/rss/cnn_topstories.rss",
+					},
+					PubDate: time.Date(2023, 4, 19, 12, 44, 51, 0, time.UTC),
+				},
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := ParseArticlesFromFeed(tt.args.urlPath)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ParseArticlesFromFeed() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got[0], tt.want[0]) {
+
+				t.Errorf("ParseArticlesFromFeed() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestParseArticlesFromFile(t *testing.T) {
+	type args struct {
+		file string
 	}
 	tests := []struct {
 		name    string
@@ -18,94 +67,80 @@ func TestParseArticlesFromFiles(t *testing.T) {
 		wantErr bool
 	}{
 		{
-			name: "success",
+			name: "rss",
 			args: args{
-				files: []string{"testdata/rss.xml", "testdata/json.json"},
+				file: "testdata/rss.xml",
 			},
 			want: []model.Article{
 				{
 					Title:       "Article 1",
 					Link:        "http://example.com/article1",
 					Description: "This is the first article.",
-					Source:      "Sample Feed",
+					Source:      model.Source{Name: "Sample Feed"},
 					PubDate:     time.Date(2006, time.January, 2, 15, 4, 5, 0, time.UTC),
 				},
 				{
 					Title:       "Article 2",
 					Link:        "http://example.com/article2",
 					Description: "This is the second article.",
-					Source:      "Sample Feed",
-					PubDate:     time.Date(2006, time.January, 3, 15, 4, 5, 0, time.UTC),
-				},
-				{
-					Title:       "Test Title",
-					Link:        "http://testurl.com",
-					Description: "Test Description",
-					Source:      "Test Source",
-					PubDate:     time.Date(2023, 6, 4, 12, 0, 0, 0, time.UTC),
-				},
-			},
-			wantErr: false,
-		},
-		{
-			name: "one invalid file",
-			args: args{
-				files: []string{"testdata/rss.xml", "testdata/invalid.json"},
-			},
-			want: []model.Article{
-				{
-					Title:       "Article 1",
-					Link:        "http://example.com/article1",
-					Description: "This is the first article.",
-					Source:      "Sample Feed",
-					PubDate:     time.Date(2006, time.January, 2, 15, 4, 5, 0, time.UTC),
-				},
-				{
-					Title:       "Article 2",
-					Link:        "http://example.com/article2",
-					Description: "This is the second article.",
-					Source:      "Sample Feed",
+					Source:      model.Source{Name: "Sample Feed"},
 					PubDate:     time.Date(2006, time.January, 3, 15, 4, 5, 0, time.UTC),
 				},
 			},
-			wantErr: true,
-		},
-		{
-			name: "all invalid files",
-			args: args{
-				files: []string{"testdata/invalid_rss.xml", "testdata/invalid.json"},
-			},
-			want:    nil,
-			wantErr: true,
-		},
-		{
-			name: "empty files",
-			args: args{
-				files: []string{"testdata/empty_rss.xml", "testdata/empty.json"},
-			},
-			want:    nil,
 			wantErr: false,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			assert := require.New(t)
-			var got []model.Article
-			var err error
-			for _, file := range tt.args.files {
-				articles, e := ParseArticlesFromFile(file)
-				if e != nil {
-					err = e
-				} else {
-					got = append(got, articles...)
-				}
+			got, err := ParseArticlesFromFile(tt.args.file)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ParseArticlesFromFile() error = %v, wantErr %v", err, tt.wantErr)
+				return
 			}
-			if tt.wantErr {
-				assert.Error(err, "ParseArticlesFromFiles() should return an error")
-			} else {
-				assert.NoError(err, "ParseArticlesFromFiles() should not return an error")
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("ParseArticlesFromFile() got = %v, want %v", got, tt.want)
 			}
-			assert.Equal(tt.want, got, "ParseArticlesFromFiles() returned unexpected result")
+		})
+	}
+}
+
+func Test_createParser(t *testing.T) {
+	type args struct {
+		format string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    Parser
+		wantErr bool
+	}{
+		{
+			name: "rss",
+			args: args{
+				format: rssFormat,
+			},
+			want:    &rss.Parser{},
+			wantErr: false,
+		},
+		{
+			name: "json",
+			args: args{
+				format: jsonFormat,
+			},
+			want:    &json.Parser{},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := createParser(tt.args.format)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("createParser() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("createParser() got = %v, want %v", got, tt.want)
+			}
 		})
 	}
 }
