a3cfa02cf66cce8a2d8d56ccd40bdca126a98e53
Anton Chaban <anton.chaban@gmail.com>
2024-07-26 16:42:25 +0300

    Update with checkenvvars

--- cmd/news-alligator/web/main.go
+++ cmd/news-alligator/web/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	_ "github.com/antonchaban/news-aggregator/cmd/news-alligator/web/docs"
 	"github.com/antonchaban/news-aggregator/pkg/handler/web"
 	"github.com/antonchaban/news-aggregator/pkg/scheduler"
@@ -23,6 +24,19 @@ import (
 // @host https://localhost:443
 // @BasePath /articles
 
+const (
+	certFileEnvVar  = "CERT_FILE"
+	keyFileEnvVar   = "KEY_FILE"
+	portEnvVar      = "PORT"
+	dbHostEnvVar    = "DB_HOST"
+	dbPortEnvVar    = "DB_PORT"
+	dbUserEnvVar    = "DB_USERNAME"
+	dbPassEnvVar    = "DB_PASSWORD"
+	dbNameEnvVar    = "DB_NAME"
+	dbSSLModeEnvVar = "DB_SSLMODE"
+	storTypeEnvVar  = "STORAGE_TYPE"
+)
+
 func main() {
 	os.Setenv("DB_HOST", "localhost")
 	os.Setenv("DB_PORT", "5436")
@@ -31,14 +45,22 @@ func main() {
 	os.Setenv("DB_NAME", "postgres")
 	os.Setenv("DB_SSLMODE", "disable")
 	os.Setenv("STORAGE_TYPE", "postgres")
+
+	if err := checkEnvVars(
+		certFileEnvVar, keyFileEnvVar, portEnvVar, dbHostEnvVar, dbPortEnvVar, dbUserEnvVar,
+		dbPassEnvVar, dbNameEnvVar, dbSSLModeEnvVar, storTypeEnvVar,
+	); err != nil {
+		logrus.Fatal(err)
+	}
+
 	// Initialize in-memory databases
 	db, err := storage.NewPostgresDB(storage.Config{
-		Host:     os.Getenv("DB_HOST"),
-		Port:     os.Getenv("DB_PORT"),
-		Username: os.Getenv("DB_USERNAME"),
-		Password: os.Getenv("DB_PASSWORD"),
-		DBName:   os.Getenv("DB_NAME"),
-		SSLMode:  os.Getenv("DB_SSLMODE"),
+		Host:     os.Getenv(dbHostEnvVar),
+		Port:     os.Getenv(dbPortEnvVar),
+		Username: os.Getenv(dbUserEnvVar),
+		Password: os.Getenv(dbPassEnvVar),
+		DBName:   os.Getenv(dbNameEnvVar),
+		SSLMode:  os.Getenv(dbSSLModeEnvVar),
 	})
 
 	//artDb := inmemory.New()
@@ -52,11 +74,11 @@ func main() {
 	h := web.NewHandler(asvc, ssvc)
 
 	// Create a new HTTPS server
-	srv := server.NewServer(os.Getenv("CERT_FILE"), os.Getenv("KEY_FILE"))
+	srv := server.NewServer(os.Getenv(certFileEnvVar), os.Getenv(keyFileEnvVar))
 
 	// Start the server in a goroutine
 	go func() {
-		if err := srv.Run(os.Getenv("PORT"), h.InitRoutes()); err != nil {
+		if err := srv.Run(os.Getenv(portEnvVar), h.InitRoutes()); err != nil {
 			logrus.Fatal("error occurred while running http server: ", err.Error())
 		}
 	}()
@@ -92,4 +114,14 @@ func main() {
 	if err := srv.Shutdown(context.Background(), articles, sources); err != nil {
 		logrus.Errorf("error occurred on server shutting down: %s", err.Error())
 	}
+}
+
+// checkEnvVars checks if the required environment variables are set and returns an error if any are missing
+func checkEnvVars(vars ...string) error {
+	for _, v := range vars {
+		if os.Getenv(v) == "" {
+			return fmt.Errorf("environment variable %s not set", v)
+		}
+	}
+	return nil
 }
