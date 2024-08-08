d42af7ac183b581b6e5613c05b30ed5f86772b78
Anton Chaban <anton.chaban@gmail.com>
2024-08-08 13:52:32 +0300

    Add tests for source_webhook

--- operator/api/v1/source_webhook_test.go
+++ operator/api/v1/source_webhook_test.go
@@ -1,47 +1,153 @@
 /*
-Copyright 2024.
-
 Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
 */
 
 package v1
 
 import (
+	"context"
+	"time"
+
 	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"k8s.io/apimachinery/pkg/types"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 var _ = Describe("Source Webhook", func() {
 
-	Context("When creating Source under Defaulting Webhook", func() {
-		It("Should fill in the default value if a required field is empty", func() {
-
-			// TODO(user): Add your logic here
-
-		})
-	})
-
 	Context("When creating Source under Validating Webhook", func() {
-		It("Should deny if a required field is empty", func() {
+		It("Should deny if the name, short_name, or link field is empty", func() {
+			ctx := context.Background()
 
-			// TODO(user): Add your logic here
+			// Testing when the name field is empty
+			source := &Source{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test-source-invalid-name",
+					Namespace: "default",
+				},
+				Spec: SourceSpec{
+					Name:      "",
+					ShortName: "short",
+					Link:      "http://example.com/rss",
+				},
+			}
+			err := k8sClient.Create(ctx, source)
+			Expect(err).Should(HaveOccurred())
 
+			// Testing when the short_name field is empty
+			source = &Source{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test-source-invalid-shortname",
+					Namespace: "default",
+				},
+				Spec: SourceSpec{
+					Name:      "Name",
+					ShortName: "",
+					Link:      "http://example.com/rss",
+				},
+			}
+			err = k8sClient.Create(ctx, source)
+			Expect(err).Should(HaveOccurred())
+
+			// Testing when the link field is empty
+			source = &Source{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test-source-invalid-link",
+					Namespace: "default",
+				},
+				Spec: SourceSpec{
+					Name:      "Name",
+					ShortName: "short",
+					Link:      "",
+				},
+			}
+			err = k8sClient.Create(ctx, source)
+			Expect(err).Should(HaveOccurred())
 		})
 
-		It("Should admit if all required fields are provided", func() {
+		It("Should deny if the name or short_name field is longer than 20 characters", func() {
+			ctx := context.Background()
+			source := &Source{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test-source-long-name",
+					Namespace: "default",
+				},
+				Spec: SourceSpec{
+					Name:      "asdbhasdbhsadjsdhfaskdasdasdasdhvkjadskhads",
+					ShortName: "short",
+					Link:      "http://example.com/rss",
+				},
+			}
+			err := k8sClient.Create(ctx, source)
+			Expect(err).Should(HaveOccurred())
 
-			// TODO(user): Add your logic here
+			source = &Source{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test-source-long-shortname",
+					Namespace: "default",
+				},
+				Spec: SourceSpec{
+					Name:      "Name",
+					ShortName: "asdbhasdbhsadjsdhfaskdasdasdasdhvkjadskhads",
+					Link:      "http://example.com/rss",
+				},
+			}
+			err = k8sClient.Create(ctx, source)
+			Expect(err).Should(HaveOccurred())
+		})
 
+		It("Should deny if the link field is not a valid URL", func() {
+			ctx := context.Background()
+
+			source := &Source{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test-source-invalid-url",
+					Namespace: "default",
+				},
+				Spec: SourceSpec{
+					Name:      "Name",
+					ShortName: "short",
+					Link:      "invalid-url",
+				},
+			}
+			err := k8sClient.Create(ctx, source)
+			Expect(err).Should(HaveOccurred())
+		})
+
+		It("Should admit if all required fields are provided and valid", func() {
+			ctx := context.Background()
+
+			source := &Source{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test-source-valid",
+					Namespace: "default",
+				},
+				Spec: SourceSpec{
+					Name:      "ValidName",
+					ShortName: "ValidShortName",
+					Link:      "http://example.com/rss",
+				},
+			}
+
+			Expect(k8sClient.Create(ctx, source)).Should(Succeed())
+
+			createdSource := &Source{}
+			Expect(k8sClient.Get(ctx, types.NamespacedName{Name: "test-source-valid", Namespace: "default"}, createdSource)).Should(Succeed())
 		})
 	})
 
+	AfterEach(func() {
+		ctx := context.Background()
+		names := []string{"test-source-invalid-name", "test-source-invalid-shortname", "test-source-invalid-link", "test-source-long-name", "test-source-long-shortname", "test-source-invalid-url", "test-source-valid"}
+		for _, name := range names {
+			source := &Source{}
+			err := k8sClient.Get(ctx, types.NamespacedName{Name: name, Namespace: "default"}, source)
+			if err == nil {
+				k8sClient.Delete(ctx, source)
+			}
+		}
+		time.Sleep(1 * time.Second)
+	})
 })
