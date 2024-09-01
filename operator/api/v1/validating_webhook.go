0a4385bf852063ce3e31d0fec3a89169b5bf4f5a
Anton Chaban <anton.chaban@gmail.com>
2024-09-01 22:04:16 +0300

    Implement validation for sources in webhook

--- operator/api/v1/validating_webhook.go
+++ operator/api/v1/validating_webhook.go
@@ -6,9 +6,11 @@ import (
 	"github.com/sirupsen/logrus"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/util/validation/field"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
+	"strings"
 )
 
 // +kubebuilder:webhook:path=/validate--v1-configmap,mutating=false,failurePolicy=fail,sideEffects=None,groups="",resources=configmaps,verbs=create;update,versions=v1,name=vconfigmap.kb.io,admissionReviewVersions=v1
@@ -22,12 +24,50 @@ type CfgMapValidatorWebHook struct {
 // validate admits a pod if a specific annotation exists.
 func (v *CfgMapValidatorWebHook) validate(ctx context.Context, obj runtime.Object) (admission.Warnings, error) {
 	cm, ok := obj.(*corev1.ConfigMap)
+	var allErrs field.ErrorList
+	var warnings admission.Warnings
 	if !ok {
 		return nil, fmt.Errorf("expected a Config Map but got a %T", obj)
 	}
 
 	logrus.Println("Validating ConfigMap", cm.Name)
-	// todo add validation logic here
+	if cm.Data == nil || len(cm.Data) == 0 {
+		return nil, fmt.Errorf("ConfigMap %s has no data", cm.Name)
+	} else {
+		var sourceList SourceList
+		err := v.Client.List(ctx, &sourceList, &client.ListOptions{Namespace: v.CfgMapNamespace})
+		if err != nil {
+			return nil, fmt.Errorf("failed to list sources: %v", err)
+		}
+
+		logrus.Println("ConfigMap has data, beginning validation")
+		for key, value := range cm.Data {
+			logrus.Printf("Key: %s, Value: %s\n", key, value)
+			sources := strings.Split(value, ",")
+			logrus.Println("Sources: ", sources)
+			if len(sources) == 0 {
+				allErrs = append(allErrs, field.Invalid(field.NewPath(key), value, "No sources found"))
+			} else {
+				logrus.Printf("ConfigMap %s has sources: %v\n", cm.Name, sources)
+				for _, sourceName := range sources {
+					found := false
+					for _, source := range sourceList.Items {
+						if source.Spec.ShortName == sourceName {
+							found = true
+							break
+						}
+					}
+					if !found {
+						allErrs = append(allErrs, field.Invalid(field.NewPath(key), value, "Source "+sourceName+" not found"))
+					}
+				}
+			}
+		}
+
+		if len(allErrs) > 0 {
+			return warnings, fmt.Errorf("validation failed: %v", allErrs)
+		}
+	}
 	return nil, nil
 }
 
@@ -36,13 +76,13 @@ func (v *CfgMapValidatorWebHook) ValidateCreate(ctx context.Context, obj runtime
 	return v.validate(ctx, obj)
 }
 
-func (v *CfgMapValidatorWebHook) ValidateUpdate(ctx context.Context, oldObj, newObj runtime.Object) (admission.Warnings, error) {
+func (v *CfgMapValidatorWebHook) ValidateUpdate(ctx context.Context, _, newObj runtime.Object) (admission.Warnings, error) {
 	logrus.Println("Validating ConfigMap Update")
 	return v.validate(ctx, newObj)
 }
 
 func (v *CfgMapValidatorWebHook) ValidateDelete(ctx context.Context, obj runtime.Object) (admission.Warnings, error) {
-	return v.validate(ctx, obj)
+	return nil, nil
 }
 
 func (v *CfgMapValidatorWebHook) SetupWebhookWithManager(mgr ctrl.Manager) error {
