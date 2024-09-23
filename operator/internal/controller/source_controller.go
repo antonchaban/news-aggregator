f75a9f3132f27c6aa1a3a8fdcd45eeab14b2340a
Anton Chaban <anton.chaban@gmail.com>
2024-09-23 23:41:36 +0300

    Add docs for source_controller.go

--- operator/internal/controller/source_controller.go
+++ operator/internal/controller/source_controller.go
@@ -19,40 +19,41 @@ import (
 	"time"
 )
 
-// SourceReconciler reconciles a Source object
+// SourceReconciler reconciles a Source object.
 // It contains the logic for handling Source resources, including creating, updating, and deleting them.
 type SourceReconciler struct {
-	Client                      client.Client   // Client for interacting with the Kubernetes API
-	Scheme                      *runtime.Scheme // Scheme for the reconciler
-	HTTPClient                  *http.Client    // HTTP client for making external requests
-	NewsAggregatorSrcServiceURL string          // URL of the news aggregator source service
+	Client                      client.Client   // Client for interacting with the Kubernetes API.
+	Scheme                      *runtime.Scheme // Scheme for the reconciler.
+	HTTPClient                  *http.Client    // HTTP client for making external requests.
+	NewsAggregatorSrcServiceURL string          // URL of the news aggregator source service.
 }
 
 const (
-	SrcFinalizer     = "source.finalizers.teamdev.com" // Finalizer string for Source resources
-	ReasonFailedUpd  = "FailedUpdate"                  // Reason for a failed update
-	ReasonFailedCr   = "FailedCreation"                // Reason for a failed creation
-	ReasonSuccessCr  = "SuccessfulCreation"            // Reason for a successful creation
-	ReasonSuccessUpd = "SuccessfulUpdate"              // Reason for a successful update
+	SrcFinalizer     = "source.finalizers.teamdev.com" // Finalizer string for Source resources.
+	ReasonFailedUpd  = "FailedUpdate"                  // Reason for a failed update.
+	ReasonFailedCr   = "FailedCreation"                // Reason for a failed creation.
+	ReasonSuccessCr  = "SuccessfulCreation"            // Reason for a successful creation.
+	ReasonSuccessUpd = "SuccessfulUpdate"              // Reason for a successful update.
 )
-
-// +kubebuilder:rbac:groups=aggregator.com.teamdev,resources=sources,verbs=get;list;watch;create;update;patch;delete
-// +kubebuilder:rbac:groups=aggregator.com.teamdev,resources=sources/status,verbs=get;update;patch
-// +kubebuilder:rbac:groups=aggregator.com.teamdev,resources=sources/finalizers,verbs=update
 
 // Reconcile is the main logic for reconciling a Source resource.
 // It handles creating, updating, and deleting sources in the news aggregator service.
+// - Retrieves the Source resource specified in the reconcile request.
+// - If the resource is marked for deletion, it calls the deleteSource method and removes the finalizer.
+// - If the resource does not exist in the news aggregator, it creates it.
+// - If the resource exists, it updates the resource in the news aggregator.
 func (r *SourceReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	var source aggregatorv1.Source
 	err := r.Client.Get(ctx, req.NamespacedName, &source)
 	if err != nil {
 		if errors.IsNotFound(err) {
-			logrus.Info("Source resource not found, possibly deleted. Removing source from news-aggregator.")
+			logrus.Info("Source resource not found, possibly deleted.")
 			return ctrl.Result{}, nil
 		}
 		return ctrl.Result{}, err
 	}
 
+	// Handle resource finalization logic
 	if source.ObjectMeta.DeletionTimestamp.IsZero() {
 		if !slices.Contains(source.Finalizers, SrcFinalizer) {
 			source.Finalizers = append(source.Finalizers, SrcFinalizer)
@@ -76,17 +77,23 @@ func (r *SourceReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctr
 
 	logrus.Info("Reconciling Source", "ID", source.Status.ID, "Name", source.Spec.Name, "Link", source.Spec.Link)
 
+	// If source ID is not set, create a new source in the news aggregator
 	if source.Status.ID == 0 {
 		return r.createSource(ctx, &source)
 	} else {
+		// Update the existing source in the news aggregator
 		return r.updateSource(ctx, source.Status.ID, &source)
 	}
 }
 
 // createSource creates a new source in the news aggregator service due to a new Source resource being created.
+// - Marshals the SourceSpec into JSON and sends a POST request to the news aggregator service.
+// - Updates the status of the Source resource based on the response.
+// - Sets the Source ID in the Status field to keep track of the resource in the external service.
 func (r *SourceReconciler) createSource(ctx context.Context, source *aggregatorv1.Source) (ctrl.Result, error) {
 	logrus.Println("Creating source:", source.Spec.Name)
 
+	// Marshal source spec into JSON
 	sourceData, err := json.Marshal(source.Spec)
 	if err != nil {
 		errUp := r.updateSourceStatus(ctx, source, aggregatorv1.SourceAdded, metav1.ConditionFalse, ReasonFailedCr, err.Error())
@@ -97,6 +104,7 @@ func (r *SourceReconciler) createSource(ctx context.Context, source *aggregatorv
 	}
 	logrus.Println("Source data:", string(sourceData))
 
+	// Create an HTTP POST request to the news aggregator service
 	req, err := http.NewRequest(http.MethodPost, r.NewsAggregatorSrcServiceURL, bytes.NewBuffer(sourceData))
 	if err != nil {
 		errUp := r.updateSourceStatus(ctx, source, aggregatorv1.SourceAdded, metav1.ConditionFalse, ReasonFailedCr, err.Error())
@@ -107,6 +115,7 @@ func (r *SourceReconciler) createSource(ctx context.Context, source *aggregatorv
 	}
 	req.Header.Set("Content-Type", "application/json")
 
+	// Send the HTTP request
 	resp, err := r.HTTPClient.Do(req)
 	if err != nil {
 		errUp := r.updateSourceStatus(ctx, source, aggregatorv1.SourceAdded, metav1.ConditionFalse, ReasonFailedCr, err.Error())
@@ -117,6 +126,7 @@ func (r *SourceReconciler) createSource(ctx context.Context, source *aggregatorv
 	}
 	defer resp.Body.Close()
 
+	// Handle non-OK status codes
 	if resp.StatusCode != http.StatusOK {
 		errUp := r.updateSourceStatus(ctx, source, aggregatorv1.SourceAdded, metav1.ConditionFalse, ReasonFailedCr, resp.Status)
 		if errUp != nil {
@@ -125,6 +135,7 @@ func (r *SourceReconciler) createSource(ctx context.Context, source *aggregatorv
 		return ctrl.Result{}, fmt.Errorf("failed to create source in news aggregator: %s", resp.Status)
 	}
 
+	// Parse the response body into a new SourceSpec
 	var createdSource aggregatorv1.SourceSpec
 	if err := json.NewDecoder(resp.Body).Decode(&createdSource); err != nil {
 		errUp := r.updateSourceStatus(ctx, source, aggregatorv1.SourceAdded, metav1.ConditionFalse, ReasonFailedCr, err.Error())
@@ -134,11 +145,13 @@ func (r *SourceReconciler) createSource(ctx context.Context, source *aggregatorv
 		return ctrl.Result{}, err
 	}
 
+	// Update the Source resource status with the new ID
 	source.Status.ID = createdSource.Id
 	if err := r.Client.Status().Update(ctx, source); err != nil {
 		return ctrl.Result{}, err
 	}
 
+	// Update the Source status with a successful creation condition
 	err = r.updateSourceStatus(ctx, source, aggregatorv1.SourceAdded, metav1.ConditionTrue, ReasonSuccessCr, "Source successfully created in news aggregator")
 	if err != nil {
 		return ctrl.Result{}, err
@@ -149,9 +162,12 @@ func (r *SourceReconciler) createSource(ctx context.Context, source *aggregatorv
 }
 
 // updateSource updates an existing source in the news aggregator service due to a Source resource being updated.
+// - Marshals the updated SourceSpec into JSON and sends a PUT request to the news aggregator service.
+// - Updates the status of the Source resource based on the response.
 func (r *SourceReconciler) updateSource(ctx context.Context, sourceID int, source *aggregatorv1.Source) (ctrl.Result, error) {
 	logrus.Println("Updating source:", source.Spec.Name)
 
+	// Marshal source spec into JSON
 	sourceData, err := json.Marshal(source.Spec)
 	if err != nil {
 		errUp := r.updateSourceStatus(ctx, source, aggregatorv1.SourceUpdated, metav1.ConditionFalse, ReasonFailedUpd, err.Error())
@@ -161,6 +177,7 @@ func (r *SourceReconciler) updateSource(ctx context.Context, sourceID int, sourc
 		return ctrl.Result{}, err
 	}
 
+	// Create an HTTP PUT request to update the source in the news aggregator service
 	url := fmt.Sprintf("%s/%d", r.NewsAggregatorSrcServiceURL, sourceID)
 	req, err := http.NewRequest(http.MethodPut, url, bytes.NewBuffer(sourceData))
 	if err != nil {
@@ -172,6 +189,7 @@ func (r *SourceReconciler) updateSource(ctx context.Context, sourceID int, sourc
 	}
 	req.Header.Set("Content-Type", "application/json")
 
+	// Send the HTTP request
 	resp, err := r.HTTPClient.Do(req)
 	if err != nil {
 		errUp := r.updateSourceStatus(ctx, source, aggregatorv1.SourceUpdated, metav1.ConditionFalse, ReasonFailedUpd, err.Error())
@@ -182,15 +200,16 @@ func (r *SourceReconciler) updateSource(ctx context.Context, sourceID int, sourc
 	}
 	defer resp.Body.Close()
 
+	// Handle non-OK status codes
 	if resp.StatusCode != http.StatusOK {
 		errUp := r.updateSourceStatus(ctx, source, aggregatorv1.SourceUpdated, metav1.ConditionFalse, ReasonFailedUpd, resp.Status)
 		if errUp != nil {
 			return ctrl.Result{}, errUp
 		}
-		logrus.Error(fmt.Errorf("failed to update source in news aggregator"), "Status", resp.Status)
 		return ctrl.Result{}, fmt.Errorf("failed to update source in news aggregator: %s", resp.Status)
 	}
 
+	// Update the Source status with a successful update condition
 	err = r.updateSourceStatus(ctx, source, aggregatorv1.SourceUpdated, metav1.ConditionTrue, ReasonSuccessUpd, "Source successfully updated in news aggregator")
 	if err != nil {
 		return ctrl.Result{}, err
@@ -201,9 +220,11 @@ func (r *SourceReconciler) updateSource(ctx context.Context, sourceID int, sourc
 }
 
 // deleteSource deletes a source from the news aggregator service.
+// - Sends a DELETE request to the news aggregator service using the provided source ID.
 func (r *SourceReconciler) deleteSource(sourceID int) (ctrl.Result, error) {
 	logrus.Println("Deleting source with ID:", sourceID)
 
+	// Create an HTTP DELETE request to remove the source from the news aggregator service
 	url := fmt.Sprintf("%s/%d", r.NewsAggregatorSrcServiceURL, sourceID)
 	req, err := http.NewRequest(http.MethodDelete, url, nil)
 	if err != nil {
@@ -211,14 +232,15 @@ func (r *SourceReconciler) deleteSource(sourceID int) (ctrl.Result, error) {
 	}
 	req.Header.Set("Content-Type", "application/json")
 
+	// Send the HTTP request
 	resp, err := r.HTTPClient.Do(req)
 	if err != nil {
 		return ctrl.Result{}, err
 	}
 	defer resp.Body.Close()
 
+	// Handle non-OK status codes
 	if resp.StatusCode != http.StatusOK {
-		logrus.Error(fmt.Errorf("failed to delete source from news aggregator"), "Status", resp.Status)
 		return ctrl.Result{}, fmt.Errorf("failed to delete source from news aggregator: %s", resp.Status)
 	}
 
@@ -227,6 +249,8 @@ func (r *SourceReconciler) deleteSource(sourceID int) (ctrl.Result, error) {
 }
 
 // updateSourceStatus updates the SourceStatus of a source resource with the given condition.
+// - Adds a new SourceCondition to the SourceStatus.Conditions list.
+// - Updates the status of the Source resource in the Kubernetes API.
 func (r *SourceReconciler) updateSourceStatus(ctx context.Context, source *aggregatorv1.Source, conditionType aggregatorv1.SourceConditionType, status metav1.ConditionStatus, reason, message string) error {
 	newCondition := aggregatorv1.SourceCondition{
 		Type:           conditionType,
@@ -240,19 +264,21 @@ func (r *SourceReconciler) updateSourceStatus(ctx context.Context, source *aggre
 	return r.Client.Status().Update(ctx, source)
 }
 
-// SetupWithManager sets up the controller with the Manager, uses predicates to filter events.
+// SetupWithManager sets up the controller with the Manager and uses predicates to filter events.
+// - Registers the controller with the Manager for the Source resource.
+// - Adds event filters to optimize reconciliation by reacting only to relevant events.
 func (r *SourceReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&aggregatorv1.Source{}).
 		WithEventFilter(predicate.Funcs{
 			CreateFunc: func(e event.CreateEvent) bool {
-				return true
+				return true // Trigger reconciliation on create events
 			},
 			DeleteFunc: func(e event.DeleteEvent) bool {
-				return !e.DeleteStateUnknown
+				return !e.DeleteStateUnknown // Trigger reconciliation if the delete state is known
 			},
 			UpdateFunc: func(e event.UpdateEvent) bool {
-				return e.ObjectNew.GetGeneration() != e.ObjectOld.GetGeneration()
+				return e.ObjectNew.GetGeneration() != e.ObjectOld.GetGeneration() // Trigger reconciliation on spec changes
 			},
 		}).
 		Complete(r)
