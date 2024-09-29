12a4354db928e1037cce95344390483ab6a054ff
Anton Chaban <anton.chaban@gmail.com>
2024-09-29 11:22:07 +0300

    Add annotation for rbac source access

--- operator/internal/controller/hotnews_controller.go
+++ operator/internal/controller/hotnews_controller.go
@@ -40,6 +40,7 @@ const HotNewsFinalizer = "hotnews.finalizers.teamdev.com"
 // +kubebuilder:rbac:groups=aggregator.com.teamdev,resources=hotnews/status,verbs=get;update;patch
 // +kubebuilder:rbac:groups=aggregator.com.teamdev,resources=hotnews/finalizers,verbs=update
 // +kubebuilder:rbac:groups=core,resources=configmaps,verbs=get;list;watch
+// +kubebuilder:rbac:groups=aggregator.com.teamdev,resources=sources,verbs=get;list;watch;create;update;patch;delete
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
